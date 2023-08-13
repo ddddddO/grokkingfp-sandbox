b2616eb298c43ae6cf34a710d9dfaebfc471e0d0
ddddddO <lbfdeatq@gmail.com>
2023-08-13 17:37:45 +0900

    modify

--- 4_17.scala
+++ 4_17.scala
@@ -1,14 +1,15 @@
-def rankedWordsWithContainS(words: List[String]): List[String] = {
-  def score(word: String): Int = {
-    word.count(_ == 's')
-  }
-  rankedWords(score, words)
-}
-
-// scala> rankedWordsWithContainS(List("rust", "ada")) == List("ada", "rust")
-// val res13: Boolean = true
-
 def rankedWords(score: String => Int, words: List[String]): List[String] = {
   words.sortBy(score)
+}
+
+// scala> rankedWords(w => score(w) + bonus(w), List("ada", "rust", "java", "scala", "haskell"))
+// val res17: List[String] = List(ada, java, rust, haskell, scala)
+
+def score(word: String): Int = {
+  word.count(_ == 's')
+}
+
+def bonus(word: String): Int = {
+  if (word.contains("c")) 5 else 0
 }
 
